4ec329cd43101de9af209a031cc74092ebdf833c
IgorCavalcantiCianniPrado <igorcianni@yahoo.com.br>
2023-05-12 10:06:18 -0300

    Primeiro commit

---
+++ FluxoCaixa/FluxoCaixa/FluxoCaixa/Controllers/FluxoCaixaController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FluxoCaixa.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FluxoCaixaController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Debito()
+        {
+            return Ok("Débito realizado.");
+        }
+
+        [HttpPost]
+        public IActionResult Credito()
+        {
+            return Ok("Crédito realizado.");
+        }
+    }
+}
---
+++ FluxoCaixa/FluxoCaixa/Relatorio/Controllers/RelatorioController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Relatorio.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RelatorioController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult SaldoConsolidado()
+        {
+            return Ok();
+        }
+    }
+}
